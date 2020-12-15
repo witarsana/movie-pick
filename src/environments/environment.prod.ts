62871155f7ec2fa1f1372044c940d9eae8d49cce
Witarsana <made.witarsana@gmail.com>
2020-12-15 15:18:24 +0800

    Update environment for production and modify in parent html

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiKey: '1b869b3ccf57d089047ded4b1de007b8',
   apiBaseUrl: 'https://api.themoviedb.org/3',
-  imageBaseUrl: 'http://image.tmdb.org/t/p/',
+  imageBaseUrl: 'https://image.tmdb.org/t/p/w370_and_h556_bestv2/',
   imageBaseDrop: 'https://image.tmdb.org/t/p/original/',
 };
